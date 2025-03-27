300481cbaf47aa12e08761a3033b575be9d49a8e
Daniel Barrionuevo Gomes <106540072+Cawlino@users.noreply.github.com>
2025-03-26 21:29:05 -0300

    Delete produto-api directory

--- produto-api/src/main/java/com/exemplo/produtoapi/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exemplo.produtoapi.repository;
-
-import com.exemplo.produtoapi.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
